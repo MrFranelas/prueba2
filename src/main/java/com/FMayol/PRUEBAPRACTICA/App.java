a943c346a0388e2d953195aa3812c26ee2860c20
MrFranelas <96344636+MrFranelas@users.noreply.github.com>
2022-01-01 16:54:18 +0100

    Add files via upload

---
+++ src/main/java/com/FMayol/PRUEBAPRACTICA/App.java
@@ -0,0 +1,13 @@
+package com.FMayol.PRUEBAPRACTICA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
